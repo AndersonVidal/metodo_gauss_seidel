cb5c2597c82f280007cb2c755f1a1979f47c6948
Tharyck Souto Vasconcelos <tharycksouto@gmail.com>
2017-06-20 21:09:52 -0300

    testeSassenfeld

--- fisica.py
+++ fisica.py
@@ -21,9 +21,23 @@ def obterSistemaTrabalho(matrizA, matrizB):
 			matrizA[i][j] = matrizA[i][j] / diagonal
 		matrizB[i] = matrizB[i] / diagonal
 
-def testeSassenfeld(matriz):
-	beta = 0
-	
+def testeSassenfeld(matriz, vetorTermosIndependentes):
+	maior = 0
+	element = 1
+	soma = 0
+
+	#percorre a matriz
+	for i in range(len(matriz)):
+		for j in range(len(matriz)):
+			soma += matriz[i][j]*element
+			print "soma", soma-1
+		if(maior != 0):
+			element = maior
+
+		if(soma > maior):
+			maior = soma
+		soma = 0
+		print "maior ", maior
 
 #COLETA NUMERO DE LINHAS
 numMalhas = int(raw_input('Digite o número de malhas do circuito:\n'))
@@ -37,7 +51,7 @@ print "das variáveis! (Ex.: i1, i2, i3,..., b1)\n"
 
 #COLETA DE DADOS DO USUÁRIO
 for i in range(numMalhas):
-	entrada = map(float, raw_input(' linha ' + str(i + 1) + ':').split())
+	entrada = map(float, raw_input(' linha ' + str(i + 1) + ':').split(" "))
 	matrizCoeficientes.append(entrada[:(len(entrada) - 1)])
 	vetorTermosIndependentes.append(entrada[(len(entrada) - 1)])
 
@@ -47,5 +61,6 @@ imprimeMatriz(matrizCoeficientes, vetorTermosIndependentes) # teste
 
 obterSistemaTrabalho(matrizCoeficientes, vetorTermosIndependentes)
 imprimeMatriz(matrizCoeficientes, vetorTermosIndependentes) #teste
+testeSassenfeld(matrizCoeficientes, vetorTermosIndependentes)
 
 #Critério matriz diagonal dominante
